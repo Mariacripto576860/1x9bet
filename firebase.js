74a90e37a6cd06d052401cf70abda0d46d83f883
CryptoStars CST <cryptostars.cst@gmail.com>
2025-06-22 17:22:40 -0300

    Corrigido firebaseConfig com apiKey válida

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// 🔥 Substitua por seus dados reais do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDgPfr1qs-82Y6c9WZFcq972A_Pa9tz3g4",
+  apiKey: "AIzaSyB4Yq1rs-dENt2bVMImAakVkaDgKfXok0Q",
   authDomain: "x9bet-7eaaf.firebaseapp.com",
   projectId: "x9bet-7eaaf",
   storageBucket: "x9bet-7eaaf.appspot.com",
-  messagingSenderId: "199779028179",
-  appId: "1:199779028179:web:b78eeb22ff35af31834326"
+  messagingSenderId: "849779018799",
+  appId: "1:849779018799:web:b7beeb22ff35af43183426"
 };
 
-// Inicializa Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
